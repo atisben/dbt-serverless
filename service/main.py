3df39573219b6fb6e6a101c5b7ed12d9e03782d8
Bastien Chappis <chappis.bastien@gmail.com>
2022-09-22 09:30:09 +0200

    removed terraform files

--- service/main.py
+++ service/main.py
@@ -3,30 +3,9 @@ from flask import Flask, render_template, request
 import subprocess
 import os
 import json
-from google.auth import jwt
 from datetime import date, timedelta
 import yaml
 
-
-def receive_authorized_get_request(request):
-    """
-    receive_authorized_get_request takes the "Authorization" header from a
-    request, decodes it using the google-auth client library, and returns
-    back the email from the header to the caller.
-    """
-    auth_header = request.headers.get("Authorization")
-    if auth_header:
-
-        # split the auth type and value from the header.
-        auth_type, creds = auth_header.split(" ", 1)
-
-        if auth_type.lower() == "bearer":
-            claims = jwt.decode(creds, verify=False)
-            print(f"Hello, {claims['email']}!\n")
-
-        else:
-            print(f"Unhandled header format ({auth_type}).\n")
-    return "Hello, anonymous user.\n"
 
 app = Flask(__name__)
 
